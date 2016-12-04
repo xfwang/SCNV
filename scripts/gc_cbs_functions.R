d5e0e593a8fd13e35cda108082310c8391f9b890
XF WANG <pwxfor@gmail.com>
2016-12-04 00:13:14 -0500

    Update and rename gc_cbs.R to gc_cbs_functions.R

--- scripts/gc_cbs_functions.R
+++ scripts/gc_cbs_functions.R
@@ -1,5 +1,11 @@
 ##Initial segmenation and copy number estimation (CBS--Circular binary Segmentation)
 ##The following fucntions are wrapper functions modified based on Baslan 2012 Nat Protol-- SRR054616.cbs.r & SRR054616.copynumber.r
+##The functions below can produce CBS results based on the coverage matrix.
+
+#gn.norm : function to perform GC normalziation
+#CBS :  wrapper function to conduct CBS segmentation
+#plot.baslan:  Plot the segmentation as in Baslan 2012
+#cne : A function to estiamte copy number and plot the desnity of segment value differences. modified from the Baslan code
 
 
 ##GC normalizaiton
@@ -15,14 +21,17 @@ gc.norm<-function (badbins=NA, gc.data, coverage) {
 			return(exp(log(jtky) - jtkz$y))
 	}
 
-	gc<-read.table(gc.data,header=T)
+	#gc<-read.table(gc.data,header=T)
+	gc<-gc.data
 	if (is.na(badbins)){bad<-0 } else {bad <-read.table(bad.bins, header=F)}
-	
-	thisRatio <- read.table(coverage, header=T)[,c("chr","start","abs.pos","coverage")]
+
+	#thisRatio <- read.table(coverage, header=T)[,c("chr","start","abs.pos","coverage")]
+	thisRatio<-coverage
+	if (nrow(gc)!= nrow(thisRatio)) {stop("files not matched")}
 	names(thisRatio) <- c("chrom", "chrompos", "abspos", "bincount")
 	a <- thisRatio$bincount + 1  #+1?
 	thisRatio$ratio <- a / mean(a)
-    
+
 	if (all.equal(gc$bin.start.abspos, thisRatio$abspos)!=T){warning("files not matched") }
 	thisRatio$gc.content <- gc$gc.content
 	thisRatio$lowratio <- lowess.gc(thisRatio$gc.content, thisRatio$ratio)
@@ -35,12 +44,12 @@ gc.norm<-function (badbins=NA, gc.data, coverage) {
 CBS<-function(thisRatio, sample.id="cell", alpha=0.05, nperm=1000, undo.SD=1.0, min.width=5) {
   #sample.id="cell";alpha=0.05; nperm=1000; undo.SD=1.0; min.width=5
   require("DNAcopy")
-  	CNA.object <- CNA(log(thisRatio$lowratio, base=2), thisRatio$chrom, thisRatio$chrompos, 
+  	CNA.object <- CNA(log(thisRatio$lowratio, base=2), thisRatio$chrom, thisRatio$chrompos,
 		data.type="logratio", sampleid=sample.id)
-	smoothed.CNA.object <- smooth.CNA(CNA.object) 
-	segment.smoothed.CNA.object <- segment(smoothed.CNA.object, alpha=alpha, nperm=nperm, undo.splits="sdundo", undo.SD=undo.SD, min.width=min.width) 
+	smoothed.CNA.object <- smooth.CNA(CNA.object)
+	segment.smoothed.CNA.object <- segment(smoothed.CNA.object, alpha=alpha, nperm=nperm, undo.splits="sdundo", undo.SD=undo.SD, min.width=min.width)
 	thisShort <- segment.smoothed.CNA.object[[2]]
-	m <- matrix(data=0, nrow=nrow(thisRatio), ncol=1)	
+	m <- matrix(data=0, nrow=nrow(thisRatio), ncol=1)
 	prevEnd <- 0
 	for (i in 1:nrow(thisShort)) {
 		thisStart <- prevEnd + 1
@@ -70,6 +79,7 @@ plot.baslan<-function (thisRatio, sample.id="cell") {
 	y.labels <- c("0.005", "0.020", "0.100", "0.500", "2.000")
 
 	#png(paste(sample.id, ".wg.png", sep=""), height=400, width=600)
+	#png(paste(sample.id, ".wg.png", sep=""))
 	par(mar=c(5.1,4.1,4.1,4.1))
 	plot(x=thisRatio$abspos, y=thisRatio$lowratio, log="y", main=paste(sample.id), xaxt="n", xlab="Genome Position Gb", yaxt="n", ylab="Ratio", col="#CCCCCC", cex=0.5)
 	axis(1, at=x.at, labels=x.labels)
@@ -80,12 +90,11 @@ plot.baslan<-function (thisRatio, sample.id="cell") {
 	abline(h=hlines)
 	abline(v=vlines)
 	mtext(chr.text, at = chr.at)
-	#dev.off()  
+	#dev.off()
 }
 
 
 ##copy number estimation
-##Caution: not working for diploid cells
 cne<- function (df,dfs) {
 	#df=SegLong; dfs=SegShort
 
@@ -109,8 +118,8 @@ cne<- function (df,dfs) {
 		starts <- c(starts, thisStart)
 		ends <- c(ends, thisEnd)
 		prevEnd = thisEnd
-	} 
-	
+	}
+
 	##well, basically pairwise differences between seg.mean.LOWESS...
 	amat <- matrix(data=0, nrow=1500000, ncol=1)
 	counter <- 1
@@ -124,12 +133,12 @@ cne<- function (df,dfs) {
 				counter <- counter+N
 			}
 		}
-	}
 
 	a3 <- amat[(1:counter),1]
-	#a3.95 <- sort(a3)[round(.95*counter)]
-	a3.95 <- quantile(a3,0.95)
-	a3d <- density(a3[which(a3 < a3.95)], n=1000)
+	a3.99 <- quantile(a3,0.99)
+	a3.new<-a3[which(a3 < a3.99)]
+	a3d<-density(a3.new,bw=.02) #<-bw is important here, suggest 0.01~0.05
+
 	cn0 <- a3d$x[which(peaks(as.vector(a3d$y), span=59))][1]
 	cn1 <- a3d$x[which(peaks(as.vector(a3d$y), span=59))][2]
 
@@ -142,11 +151,4 @@ cne<- function (df,dfs) {
 
   return(df)
 }
-
-
-####
-####
-NormRatio<-gc.norm(badbins=NA, gc.data="hg19.varbin.gc.content.5k.bowtie.k50.txt", coverage="cell.bin.rc")
-Seg<-CBS(NormRatio, sample.id="cell", alpha=0.05, nperm=1000, undo.SD=1.0, min.width=5)
-SegLong<-Seg[[1]]; SegShort<-Seg[[2]]; 
-plot.baslan(SegLong)
+}
